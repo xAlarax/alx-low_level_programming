1d77885d7267667c147183b38b3cf22397806b86
Inaya777 <flaffyfoo33@gmail.com>
2023-03-16 08:45:28 -0700

    advanced2

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <unistd.h>
 /**
-  * main - prints and that piece of art is useful"
-  * Return: 1 if the number is positive. 0 otherwise
-  */
+ * main - prints and that piece of art is useful"
+ * Return: 1 if the number is positive. 0 otherwise
+ */
 int main(void)
 {
 	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 
-	write(2, quote, sizeof(statement) - 1);
+	write(2, quote, sizeof(quote) - 1);
 
 	return (1);
 }

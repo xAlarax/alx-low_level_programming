54c46757e3e101ca5a1937ede4f93db9d01b7f0e
Inaya777 <flaffyfoo33@gmail.com>
2023-03-16 05:55:53 -0700

    add 3 commits

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  *main - printing "Programming is like building a multilingual puzzle
- *return: 0 if exited right, not 0 otherwise
+ *Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {

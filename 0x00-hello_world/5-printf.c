a8f57b699253b7e7c8c9d8e6d86ce9bddba86dbc
Inaya777 <flaffyfoo33@gmail.com>
2023-03-16 06:10:10 -0700

    printing

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
-  *main - printing the quote with proper grammar,
-  *but the outcome is a piece of art,
-  *return: 0 if exited properly, non-zero otherwise
-  */
+ *main - printing the quote with proper grammar,
+ *but the outcome is a piece of art,
+ *return: 0 if exited properly, non-zero otherwise
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return 0;
+	return (0);
 }

c4241e2cf5e8e1831799cf0b2ba0de182bbd2593
Inaya777 <flaffyfoo33@gmail.com>
2023-03-16 08:48:20 -0700

    advanced2

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,7 @@
  */
 int main(void)
 {
-	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	write(2, quote, sizeof(quote) - 1);
 

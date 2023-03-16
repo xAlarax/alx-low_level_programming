4ebf58cee55cfaad1126bac72dea5bc6affd0da7
Inaya777 <flaffyfoo33@gmail.com>
2023-03-16 07:43:17 -0700

    last mandatory task

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints te size of various types on the computer it is compiled and run on
+ * Return: 0 if exited properly, non 0 otherwise
+ */
+int main(void)
+{
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
+	return (0);
+}

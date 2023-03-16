2c18b2bc500c087cc6f624f805b8d05296122d3f
Inaya777 <flaffyfoo33@gmail.com>
2023-03-16 06:52:52 -0700

    printing

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,7 +4,7 @@
  *but the outcome is a piece of art,
  *return: 0 if exited properly, non 0 otherwise
  */
-int main()
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);

e0a721c121268b9a307d9719218b9a6e902a242d
Inaya777 <flaffyfoo33@gmail.com>
2023-03-16 06:14:09 -0700

    printing

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- *main - printing the quote with proper grammar,
+ *main - prints the phrase with proper grammar,
  *but the outcome is a piece of art,
  *return: 0 if exited properly, non-zero otherwise
  */

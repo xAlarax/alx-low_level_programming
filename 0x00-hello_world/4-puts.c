18f42874c0b6a8284c65e2ba813f40334c09c97b
Inaya777 <flaffyfoo33@gmail.com>
2023-03-16 05:53:17 -0700

    add 3 commits

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *main - printing "Programming is like building a multilingual puzzle
+ *return: 0 if exited right, not 0 otherwise
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+		return (0);
+}

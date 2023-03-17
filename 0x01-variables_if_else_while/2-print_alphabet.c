adb704f14863efbf7ee7af7fb0962b531d7e0aef
Inaya777 <flaffyfoo33@gmail.com>
2023-03-17 07:38:04 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+  * main - prints the alphabet in lowercase
+  * Return: 0 always success
+  */
+int main(void)
+{
+	char n;
+
+	for (n = 'a'; n <= 'z'; ++n)
+		putchar(n);
+	return (0);
+}

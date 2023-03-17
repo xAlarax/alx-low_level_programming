6a6fdf73248427720ec4c35238cdd1e541df19fb
Inaya777 <flaffyfoo33@gmail.com>
2023-03-17 12:22:21 -0700

    task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - assign a random number to the variable n each time it is executed
+ * Return: 0 always success
+ */
+int main(void)
+{
+	int n, m;
+
+	for (n = 0 ; n <= 8 ; n++)
+		for (m = 1 ; m <= 9 ; m++)
+			if (n < m)
+			{
+				putchar(n + '0');
+				putchar(n + '0');
+				if (n != 8 || m != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+	putchar('\n');
+	return (0);
+}

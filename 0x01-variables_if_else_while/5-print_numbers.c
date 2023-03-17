57c1f95ca0f6b2a1a3310dfb5d13f4b57b0a187f
Inaya777 <flaffyfoo33@gmail.com>
2023-03-17 08:45:31 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *main - prints all single digit numbers of base 10 starting from 0
+ *Return: 0 always successful
+ */
+int main(void)
+{
+	printf("0123456789");
+	return (0);
+}

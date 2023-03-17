97ad4c84dd7ae0b0f5eb719757ac78c7dbdca6b9
Inaya777 <flaffyfoo33@gmail.com>
2023-03-17 08:16:40 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+  *main - prints the alphabet in lowercase
+  *Return: 0 always success
+  */
+int main(void)
+{
+	char c;
+
+	c = 'a';
+
+	while (c <= 'z')
+		if (c != 'q' && c != 'e')
+			putchar(n);
+	c++;
+	putchar('\n');
+	return (0);
+}

592ccdb886fe188f05df667317461c942509b2e6
Inaya777 <flaffyfoo33@gmail.com>
2023-03-21 13:47:07 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+  * _abs - compute absolute value
+  * @x: character
+  * Return: the absolute value
+  */
+int _abs(int x)
+{
+	if (x < 0)
+		return (-x);
+	else
+		return (x);
+}

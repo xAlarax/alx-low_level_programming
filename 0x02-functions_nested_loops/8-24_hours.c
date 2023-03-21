d0788b9798e1e637541d887d9389038919def493
Inaya777 <flaffyfoo33@gmail.com>
2023-03-21 15:40:44 -0700

    task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,24 +5,20 @@
   */
 void jack_bauer(void)
 {
-	int i;
 	int j;
 	int n;
 
-	for (i = 0 ; i <= 1440 ; i++)
-	{
-		for (j = 0 ; j <= 23 ; j++)
+	for (j = 0 ; j <= 23 ; j++)
 		{
 			for (n = 0 ; n <= 59 ; n++)
 			{
 				_putchar((j / 10) + '0');
 				_putchar((j % 10) + '0');
 				_putchar(':');
-				_putchar((j / 10) + '0');
-				_putchar((j % 10) + '0');
+				_putchar((n / 10) + '0');
+				_putchar((n % 10) + '0');
 				_putchar('\n');
 			}
 		}
-	}
 }
 

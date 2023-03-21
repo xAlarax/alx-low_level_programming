a859872855607bed731682908b6d9b6abae5e0ae
Inaya777 <flaffyfoo33@gmail.com>
2023-03-21 10:31:15 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+  * print_alphabet - prints alphabets
+  * Return: Always 0.
+  */
+void print_alphabet(void)
+{
+	for (i = 'a'; i <= 'z' ; i++)
+		_putchar(i);
+	_putchar('\n');
+}

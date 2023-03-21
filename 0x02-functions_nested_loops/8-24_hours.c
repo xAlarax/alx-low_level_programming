a3b1b16e4735098755c3cf206c2cec692b65535c
Inaya777 <flaffyfoo33@gmail.com>
2023-03-21 15:26:24 -0700

    task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+  * jack_bauer - prints every minute of the day
+  * Return: 0 Always
+  */
+void jack_bauer(void)
+{
+	int i, j, n;
+
+	for (i = 0 ; i <= 1440 ; i++)
+	{
+		for (j = 0 ; j <= 23 ; j++)
+		{
+			for (n = 0 ; n <= 59 ; n++)
+			{
+				_putchar((j / 10) + '0');
+				_putchar((j % 10) + '0');
+				_putchar(':');
+				_putchar((j / 10) + '0');
+				_putchar((j % 10) + '0');
+				_putchar('\n');
+			}
+		}
+	}
+}
+

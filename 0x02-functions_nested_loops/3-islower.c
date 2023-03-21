65aceae68a902f8b6b868f0d551f87c209293f84
Inaya777 <flaffyfoo33@gmail.com>
2023-03-21 11:20:17 -0700

    task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+  * _islower - check for lower
+  * @c: character
+  * Return: 0 Always
+  */
+int _islower(int c)
+{
+	return (c <= 'a' && c >= 'z');
+}

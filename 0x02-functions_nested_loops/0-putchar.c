7fd2f098a919056dcdf7b96ee98f08a990a97178
Inaya777 <flaffyfoo33@gmail.com>
2023-03-21 09:24:01 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,20 @@
 #include "main.h"
+#include <stdio.h>
 /**
-  * main -  prints word _putchar
-  * Return : Always 0 success
-  */
+ * main - Print _putchar
+ * Return: Always 0 success
+ */
 int main(void)
 {
-	_putchar("_");
-	_putchar("p");
-	 _putchar("u");
-	  _putchar("t");
-	  _putchar("c");
-	  _putchar("h");
-	  _putchar("a");
-	  _putchar("r");
-	  _putchar("\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,1 +1,1 @@
-readme for funtions
+readme functions

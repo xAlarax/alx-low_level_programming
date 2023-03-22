5bb772035c5ddf101d594e089dc6702730fdf0f8
Inaya777 <flaffyfoo33@gmail.com>
2023-03-22 12:53:36 -0700

    sum of multiples

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - computes and prints the sum of all the multiples
+ * of 3 or 5 below 1024
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int sum = 0;
+	unsigned long int sum;
+
+	for (i = 1; i < 1024; i++)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+		{
+			sum += i;
+		}
+	}
+	printf("%lu\n", sum);
+	return (0);
+}

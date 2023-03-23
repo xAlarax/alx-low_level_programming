0af9f180b68aa49d0a16481e7e429b95ff32bdc3
Inaya777 <flaffyfoo33@gmail.com>
2023-03-23 11:37:14 -0700

    task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,12 @@
 #include "main.h"
 /**
  * _isupper - check uppercase
+ * @c: character
  * Return: 0 or 1
  */
 int _isupper(int c)
 {
-	if (c => 'A' && c <= 'Z')
+	if (c >= 'A' && c <= 'Z')
 		return (1);
 	else
 		return (0);
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN
-#define MAIN
+#ifndef FILE_MAIN
+#define FILE_MAIN
 
 int _putchar(char c);
 int _isupper(int c);

7d6118321288d840b6c2328a1c7882932201b56a
Inaya777 <flaffyfoo33@gmail.com>
2023-03-27 15:07:34 -0700

    all

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_rev - a function that print reverse of a string
+ * @s: pointer variable
+ * Return: void return nothing
+ */
+void print_rev(char *s)
+{
+	int i, len;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	len = i;
+	for (i = len - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}

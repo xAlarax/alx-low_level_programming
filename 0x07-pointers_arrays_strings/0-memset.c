80991c713b5c40a4d12572ef4d4c32b78f32cb08
Inaya777 <flaffyfoo33@gmail.com>
2023-04-03 08:34:55 -0700

    pointers

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /*
-  * 0-memset.c - function that concanerates strings
-  * @s: pointer variable
-  * @b: variable
-  * @n: size
-  * Return: s Always 
-  */
+ * 0-memset.c - function that concanerates strings
+ * @s: pointer variable
+ * @b: variable
+ * @n: size
+ * Return: s Always 
+ */
 char *_memset(char *s, char b, unsigned int n){
 	char *pS = s;
 	while (n > 0){

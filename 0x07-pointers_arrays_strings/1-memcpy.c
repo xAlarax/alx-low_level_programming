db6584382a8f1a6eb768cd5d1f29b8ba50b20cc6
Inaya777 <flaffyfoo33@gmail.com>
2023-04-03 11:51:42 -0700

    memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,6 +11,6 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	int i;
 
 	for (i = 0; i < n; i++)
-		*(dest + i) = *(src + i)
-			return (dest);
+		*(dest + i) = *(src + i);
+	return (dest);
 }

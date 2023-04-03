1c48481b03050776753773cf6e9a3ab5363db49e
Inaya777 <flaffyfoo33@gmail.com>
2023-04-03 11:49:29 -0700

    memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _memcpy - function that copies memory area
+ * @dest: pointer variable
+ * @src: pointer variable
+ * @n: variable
+ * Return: S Always
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		*(dest + i) = *(src + i)
+			return (dest);
+}

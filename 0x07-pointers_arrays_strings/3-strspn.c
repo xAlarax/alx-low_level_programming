efdc59cbc656769fa891062beb38f54b9e8c64f6
Inaya777 <flaffyfoo33@gmail.com>
2023-04-04 15:23:44 -0700

    task 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strspn - function that gets the lenght of a prefix substring
+ * @s: pointer variable
+ * @accept: second poiter variable
+ * Return: the number of bytes of s from accept
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int ten = strspn(s, accept);
+	return (ten);
+}

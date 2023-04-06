aaf948a2ab8ca2d09c0f55a8f4e1f6f92cdc7b74
Inaya777 <flaffyfoo33@gmail.com>
2023-04-05 19:54:44 -0700

    task 2

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _pow_recursion - prints power of number
+ * @x: number
+ * @y: the power of number
+ * Return: the value of the powered num
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	return (x * _pow_recursion(x, y - 1));
+}

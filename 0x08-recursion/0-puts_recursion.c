efcad0118f1116b45f1ae904dccc7175e3c3ada1
Inaya777 <flaffyfoo@gmail.com>
2023-06-07 10:36:04 -0700

    task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - puts string followed by new line
+ * @s: string
+ * Return: 0
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	else
+	{
+		_putchar(*s);
+		(_puts_recursion(s + 1));
+	}
+}
+

d15a17e1191bf7815ccd0767e7b1d7dc1013c3a7
Inaya777 <flaffyfoo@gmail.com>
2023-06-08 13:05:47 -0700

    task 4

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - returns the natural root of a number
+ * @n: number
+ * Return: the value
+ */
+int _sqrt_recursion(int n)
+{
+	return (_sqrt(n, 1));
+}
+/**
+ * _sqrt - second function
+ * @n: the same number
+ * @m: the variable
+ *Return: the square root
+ */
+int _sqrt(int n, int m)
+{
+	if (n < 0)
+		return (-1);
+	else if (m * m > n)
+		return (-1);
+	else if (m * m == n)
+		return (m);
+	return (_sqrt(n, m + 1));
+}

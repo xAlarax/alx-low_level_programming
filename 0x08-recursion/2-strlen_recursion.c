e85fe1488d6c3809feb033f480cd9d905ce2e132
Inaya777 <flaffyfoo@gmail.com>
2023-06-07 19:15:51 -0700

    kan3ani

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen_recursion - returns a string length
+ * @s: pointer variable
+ * Return: none
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+	else
+		return (0);
+}
+

25b9f9c4bc3fd124a4f4e794c8e78f064da69064
Inaya777 <flaffyfoo@gmail.com>
2023-06-09 16:54:30 -0700

    task 0

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main - fct
+ * @argc: num
+ * @argv: string
+ * Return: 0 or one
+ */
+int main(int argc, char *argv[])
+{
+	int i, n = 0;
+
+	if (argc == 1 || argc > 2)
+	{
+		printf("Error/n");
+		return (1);
+	}
+
+	i = atoi(argv[1]);
+
+	while (i > 0)
+	{
+		if (i >= 25)
+			i -= 25;
+		else if (i >= 10)
+			i -= 10;
+		else if (i >= 5)
+			i -= 5;
+		else if (i >= 2)
+			i -= 2;
+		else if (i >= 1)
+			i -= 1;
+		n += 1;
+	}
+	printf("%d\n", n);
+	return (0);
+}

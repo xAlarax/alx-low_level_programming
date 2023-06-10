c5151bc833166dc0fc11c5e909de94bbb604617c
Inaya777 <flaffyfoo@gmail.com>
2023-06-10 12:20:14 -0700

    task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,14 +9,15 @@
  */
 int main(int argc, char *argv[])
 {
-	int x = atoi(argv[1]);
-	int y = atoi(argv[2]);
+	int x, y;
 
 	if (argc < 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
+	x = atoi(argv[1]);
+	y = atoi(argv[2]);
 	printf("%d\n", x * y);
 	return (0);
 }

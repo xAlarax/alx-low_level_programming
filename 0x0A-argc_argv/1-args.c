64322323c5894c09088214679c74e2d21c25e702
Inaya777 <flaffyfoo@gmail.com>
2023-06-10 10:35:54 -0700

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - function
+ * @argc: num
+ * @argv: string
+ * Return: 0
+ */
+int main(int argc, char *argv[]__attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}

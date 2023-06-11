c06b1d7327da7fe471518d131c45034187323548
nope <125764403+Inaya777@users.noreply.github.com>
2023-06-11 23:33:21 +0200

    task 5

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -8,7 +8,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int cents, ncoins = 0;
+	int mon, num = 0;
 
 	if (argc == 1 || argc > 2)
 	{
@@ -16,22 +16,22 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 
-	cents = atoi(argv[1]);
+	mon = atoi(argv[1]);
 
-	while (cents > 0)
+	while (mon > 0)
 	{
-		if (cents >= 25)
-			cents -= 25;
-		else if (cents >= 10)
-			cents -= 10;
-		else if (cents >= 5)
-			cents -= 5;
-		else if (cents >= 2)
-			cents -= 2;
-		else if (cents >= 1)
-			cents -= 1;
-		ncoins += 1;
+		if (mon >= 25)
+			mon -= 25;
+		else if (mon >= 10)
+			mon -= 10;
+		else if (mon >= 5)
+			mon -= 5;
+		else if (mon >= 2)
+			mon -= 2;
+		else if (mon >= 1)
+			mon -= 1;
+		num += 1;
 	}
-	printf("%d\n", ncoins);
+	printf("%d\n", num);
 	return (0);
 }

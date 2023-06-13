2944a915f287945227d47f401296cf939bf9a57e
Inaya777 <flaffyfoo@gmail.com>
2023-06-13 14:12:27 -0700

    yaaaareeeb

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,6 +13,10 @@ char *str_concat(char *s1, char *s2)
 	int l2 = strlen(s2);
 	char *ps;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	ps = malloc((l1 + l2 + 1) * sizeof(char));
 	if (ps == NULL)
 		return (NULL);

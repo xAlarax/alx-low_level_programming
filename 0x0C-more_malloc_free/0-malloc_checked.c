fcbaaa68ea47bd282ee8356a9c3833edbe875df7
Inaya777 <flaffyfoo@gmail.com>
2023-06-16 16:51:49 -0700

    makanch 3ndi lwe9t n9elto :((

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * malloc_checked - function
+ * @b: number
+ * Return: pointer
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *pb;
+
+	pb = malloc(b);
+	if (pb == NULL)
+		exit(98);
+	return (pb);
+}

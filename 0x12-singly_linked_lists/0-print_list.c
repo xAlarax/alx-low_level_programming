5fab2a76b35485f1696d95c9acea0c1282f563d5
Inaya777 <flaffyfoo33@gmail.com>
2023-07-01 00:42:28 +0100

    task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/** print_list - function that all elements of a list_t
+ * @h: pointer
+ * Return: the number of nodes
+*/
+size_t print_list(const list_t *h)
+{
+    int num = 0;
+
+    while (h)
+    {
+        if (!h->str)
+        {
+        printf("[0] (nil)\n");
+        }
+        else
+        {
+        printf("[%d] %s\n", h->len, h->str);
+        }
+        h = h->next;
+        num++;
+    }
+    return (num);
+}

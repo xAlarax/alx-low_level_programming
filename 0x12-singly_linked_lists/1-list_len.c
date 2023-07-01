372d6d1afd80c884e3cdc5481c8f0bac1cab0725
Inaya777 <flaffyfoo33@gmail.com>
2023-07-01 02:14:49 +0100

    task 0

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * list_len - num of elements
+ * @h: constant pointer tyep list_t
+ * Return: numbers of elements in list_t
+ */
+size_t list_len(const list_t *h)
+{
+int i = 0;
+
+while (h)
+{
+i++;
+h = h->next;
+}
+return (i);
+}

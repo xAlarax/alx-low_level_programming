7bfb43de0b68dc30b7a754c583728ff13b113bb6
nope <125764403+Inaya777@users.noreply.github.com>
2023-07-04 20:24:55 +0100

    fixed locally

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,13 +1,13 @@
 #include "lists.h"
 /**
- * print_listint - fct that prints
- * @h: head
- * Return: the number of nodes
- */
+* print_listint - fct that prints
+* @h: head
+* Return: the number of nodes
+*/
 size_t print_listint(const listint_t *h)
 {
 	int num;
-
+	
 	num = 0;
 	while (h)
 	{

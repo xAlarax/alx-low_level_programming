fc5bd1c2bc612f0110471c7da4aaddf3db8b7809
Inaya777 <flaffyfoo33@gmail.com>
2023-07-06 16:06:10 -0700

    fixed setbit

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -7,11 +7,9 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int mask;
 
-	mask  = 1 << index;
-	if (index > UNSINED_INT_LENGTH || !n)
+	if (!n || index >= sizeof(unsigned long int) * 8)
 		return (-1);
-	*n = *n | mask;
+	*n |= (1UL << index);
 	return (1);
 }
